a28684adf11029bfecc8122d6282c572e634c0fd
sherzodm8441 <mirsharipovsherzod@gmail.com>
2023-09-06 19:01:17 -0400

    add some css to account for mobile screen size

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -10,6 +10,34 @@ const Chats = () => {
                 <p>Hey there</p>
             </div>
         </div>
+        <div className='userChat'>
+            <img src="https://imgv3.fotor.com/images/cover-photo-image/a-beautiful-girl-with-gray-hair-and-lucxy-neckless-generated-by-Fotor-AI.jpg" />
+            <div className='userChatInfo'>
+                <span>Display Name</span>
+                <p>Hey there</p>
+            </div>
+        </div>
+        <div className='userChat'>
+            <img src="https://imgv3.fotor.com/images/cover-photo-image/a-beautiful-girl-with-gray-hair-and-lucxy-neckless-generated-by-Fotor-AI.jpg" />
+            <div className='userChatInfo'>
+                <span>Display Name</span>
+                <p>Hey there</p>
+            </div>
+        </div>
+        <div className='userChat'>
+            <img src="https://imgv3.fotor.com/images/cover-photo-image/a-beautiful-girl-with-gray-hair-and-lucxy-neckless-generated-by-Fotor-AI.jpg" />
+            <div className='userChatInfo'>
+                <span>Display Name</span>
+                <p>Hey there</p>
+            </div>
+        </div>
+        <div className='userChat'>
+            <img src="https://imgv3.fotor.com/images/cover-photo-image/a-beautiful-girl-with-gray-hair-and-lucxy-neckless-generated-by-Fotor-AI.jpg" />
+            <div className='userChatInfo'>
+                <span>Display Name</span>
+                <p>Hey there</p>
+            </div>
+        </div>
     </div>
   )
 }
