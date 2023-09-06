0b7e4bf56e4e7e0e2c4e4063bf8798d3cbdf7e78
sherzodm8441 <mirsharipovsherzod@gmail.com>
2023-09-06 01:12:17 -0400

    add search to sidebar; close #6

---
+++ src/components/Search.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className='search'>
+        <div className='searchForm'>
+            <input type="text" placeholder='Find user'/>
+        </div>
+
+        <div className='userChat'>
+            <img src="https://imgv3.fotor.com/images/cover-photo-image/a-beautiful-girl-with-gray-hair-and-lucxy-neckless-generated-by-Fotor-AI.jpg" />
+            <div className='userChatInfro'>
+                <span>Display Name</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Search
