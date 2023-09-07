17d9cc245ed4994545e9c5eb424724a7afc79f3b
sherzodm8441 <mirsharipovsherzod@gmail.com>
2023-09-07 00:45:07 -0400

    add message and messages files :)

---
+++ src/components/Message.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div className='message self'>
+      <div className='messageInfo'>
+        <img src="https://imgv3.fotor.com/images/cover-photo-image/a-beautiful-girl-with-gray-hair-and-lucxy-neckless-generated-by-Fotor-AI.jpg"/>
+        <span>12:09</span>
+      </div>
+      <div className='messageContent'>
+        <p>hey there!</p>
+        <img src="https://imgv3.fotor.com/images/cover-photo-image/a-beautiful-girl-with-gray-hair-and-lucxy-neckless-generated-by-Fotor-AI.jpg" />
+      </div>
+    </div>
+  )
+}
+
+export default Message
