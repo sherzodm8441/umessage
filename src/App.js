b45d1b458639fd09ca01a13b5510127ab8e74874
sherzodm8441 <mirsharipovsherzod@gmail.com>
2023-09-08 00:58:19 -0400

    add react router and protect home page for when not logged in; close #15

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import './App.scss';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from './contexts/AuthContext';
 
 function App() {
+  const {currentUser} = useContext(AuthContext);
+
+  const ProtectedRoute = ({children}) => {
+    if(!currentUser){
+      return <Navigate to="/login"/>;
+    }
+
+    return <Home />
+  }
+
   return (
-    <div className="App">
-      {/* <Home /> */}
-      <Register />
-      {/* <Login /> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+            }/>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
